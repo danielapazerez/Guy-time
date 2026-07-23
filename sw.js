0458b440fcc1950b36c889b70f15a2ec154a19d5
danielapazerez <daniela@pazgroup.co.il>
2026-07-24 01:24:40 +0300

    Fix right and left buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guy-time-v1.0.0';
+const CACHE = 'guy-time-v1-2.0.0';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js','./manifest.webmanifest',
   './icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'
